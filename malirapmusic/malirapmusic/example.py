80cf42e9e60afbb2a750b33f84085605aba42717
bouctou04 <bmaiga04@gmail.com>
2020-12-13 12:48:24 +0000

    Initialisation du dépot distant

---
+++ malirapmusic/malirapmusic/example.py
@@ -0,0 +1,29 @@
+from .settings import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+# MYSQL Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_name',
+        'USER': 'db_user',
+        'PASSWORD': 'db_password',
+        'HOST': 'db_host',
+    }
+}
+
+# SQLite Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
