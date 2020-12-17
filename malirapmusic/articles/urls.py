5df7eb06d35c999d1c57c775a526d2568892f5ec
bouctou04 <bmaiga04@gmail.com>
2020-12-17 01:55:01 +0000

    Integration du téléchargement des fichiers mp3, Ajout de la fonctionnalité de recherche par catégorie et par champ de recherche, réorganisation de la page de détail des articles

--- malirapmusic/articles/urls.py
+++ malirapmusic/articles/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index, kwargs={}, name='index'),
     path('<slug:param>', views.detail, kwargs={}, name='detail'),
+    path('search/<slug:param>', views.search, kwargs={}, name='search'),
+    path('search/', views.search_form, kwargs={}, name='search_form')
 ]
--- malirapmusic/articles/views.py
+++ malirapmusic/articles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-
+from django.db.models import Q
 from .models import *
 # Create your views here.
 
@@ -20,3 +20,19 @@ def detail(request, param):
         'article': article
     }
     return render(request, 'articles/detail.html', datas)
+
+def search(request, param):
+    result = Articles.objects.filter(Q(title__icontains=param) | Q(category__fields__icontains=param))
+    datas = {
+        'results': result
+    }
+    return render(request, 'articles/search.html', datas)
+
+def search_form(request):
+    context = request.GET['s']
+    result = Articles.objects.filter(Q(title__icontains=context) | Q(category__fields__icontains=context))
+    datas = {
+        'results': result,
+        'context': context
+    }
+    return render(request, 'articles/search.html', datas)
