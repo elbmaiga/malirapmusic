963504e3a99c6166343d5c5df57c438b8f66e062
bouctou04 <bmaiga04@gmail.com>
2020-12-18 14:25:24 +0000

    Intégration des pages statiques (about.html, contact.html)

--- malirapmusic/malirapmusic/urls.py
+++ malirapmusic/malirapmusic/urls.py
@@ -20,8 +20,11 @@ import debug_toolbar
 
 from articles import views as article_views
 from articles import urls
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', views.about, kwargs={}, name='about'),
+    path('contact/', views.contact, kwargs={}, name='contact'),
     path('__debug__/', include(debug_toolbar.urls)),
     path('', include('articles.urls')),
     path('store/', include('store.urls')),
